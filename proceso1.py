c0fe1c98df57555452306e1525cedf3383a4d597
Daniel Castillo <daniel.castillo4511@gmail.com>
2022-11-04 19:44:32 -0300

    cambio 2

--- proceso1.py
+++ proceso1.py
@@ -1,19 +1,30 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import datetime
 
-
+#- Usar un archivo Excel como fuente de datos iniciales, con al menos 1000 registros. Puede ser un archivo propio o bien obtenido de alguna fuente pública, de cualquier forma, debe quedar disponible en el repositorio de entrega.
 def carga_inicial():
-  return pd.read_excel("ODC2022.xls")
+  return pd.read_excel("ODC2022.xlsx")
 
-# Se lee la información de forma óptima
 odc = carga_inicial()
 
-# Obtener parte de la información
+# Modificar algunos nombres de columnas de datos, independiente a que sean correctos, lo que se espera es la capacidad de cambiar sus nombres.
 odc_renombrado = odc.rename(columns={'TOTAL  FORESTAL':'AREA FORESTAL AFECTADA',
                                    'TOTAL OTRAS SUPERFICIES':'OTRAS AREAS AFECTADAS',
                                    'TOTAL SUPERFICIE AFECTADA':'AREA TOTAL AFECTADA'})
 
-#odc_renombrado.dropna(subset=["Comuna"], inplace=True)
-#geo_data = odc_renombrado
+#- Crear una columna de datos en el DataFrame, con la siguiente definición: “Fecha de Proceso”, donde se debe registrar la fecha en que se está procesando el Excel. Tip: La misma fecha para todos los registros en esa columna.
+fecha_actual = datetime.datetime.now()
+odc_renombrado['Fecha de Proceso'] = fecha_actual.strftime("%d-%m-%Y")
 print(odc_renombrado)
+
+#- Crear una columna vacía en el DataFrame con el nombre “Clima”, después asignarle valor de clima obtenido desde API de OpenWeatherMap, para esto aplicar una lógica a elección propia, donde se evalúen al menos 2 columnas y el dato debe provenir de al menos 5 comunas. Tip: se debe hacer la consulta de las 5 comunas antes de aplicar el dato a la columna de Clima, similar al ejemplo de horarios en la clase final.
+
+
+#- El DataFrame resultante, debe ser almacenado en una base de datos local
+
+
+#- Obtener los registros de la base de datos y exportar a un nuevo archivo Excel y otro archivo CSV.
+
+
