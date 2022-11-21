3a5f11dd1dab4c2cf89e3c51dc4362802c667722
daniel <daniel.castillo4511@gmail.com>
2022-11-21 14:44:34 -0300

    dd

--- pages/1_Filtro_Uno.py
+++ pages/1_Filtro_Uno.py
@@ -7,18 +7,6 @@ import pandas as pd
 
 # Se importan funcionalidades desde librería propia
 from utils import data_incendio
-"""
-def data_incendio():
-  # Se lee Excel de datos
-  incendios=pd.read_excel("ODC2022.xlsx")
-
-  # Obtener columnas de datos
-  #data_comuna = incendios[ ["REGION","PROVINCIA", "COMUNA", "NUMERO INCENDIOS ", "TOTAL  FORESTAL ", "TOTAL OTRAS SUPERFICIES", "TOTAL SUPERFICIE AFECTADA", "AÑO", "LATITUD", "LONGITUD"]]
-
-  # Limpiar los datos, Eliminando los registros sin Comuna
-  #data_comuna.dropna(subset=["COMUNA"], inplace=True)
-
-  return incendios"""
 
 # Obtener datos desde cache
 data_puntos = data_incendio()
